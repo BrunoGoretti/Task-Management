7b113efa32c6d1ad02287624c023cad79f631518
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-05-23 01:54:47 +0400

    Add desing

--- Frontend/my-task-management/src/components/TaskList.jsx
+++ Frontend/my-task-management/src/components/TaskList.jsx
@@ -1,27 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/tasks') 
-      .then(response => response.json())
-      .then(data => setTasks(data))
-      .catch(error => console.error(error));
+    fetch("http://localhost:5000/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error(error));
   }, []);
 
   return (
-    <div>
-      <h2>Task List</h2>
-      <ul>
-        {tasks.map(task => (
-          <li key={task._id}>
-            <strong>{task.task_name}</strong>
-            <p>{task.task_description}</p>
-            <p>Completed: {task.is_completed ? 'Yes' : 'No'}</p>
-          </li>
-        ))}
-      </ul>
+    <div className="background-container">
+      <div className="task-list-container">
+        <h2>Task List</h2>
+        <table className="task-table">
+          <thead>
+            <tr>
+              <th>Task Name</th>
+              <th>Description</th>
+              <th>Completed</th>
+              <th>Created At</th> 
+              <th>Completed At</th> 
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <tr key={task._id}>
+                <td>{task.task_name}</td>
+                <td>{task.task_description}</td>
+                <td>{task.is_completed ? "Yes" : "No"}</td>
+                <td>{task.created_at}</td> 
+                <td>{task.completed_at}</td> 
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
